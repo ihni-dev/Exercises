24b444ac0a69bff8de116d9643a978b700cea417
Marek <frankowicz.marek@gmail.com>
2019-03-24 14:26:02 +0100

    Implement TryFindIndicies method

--- TwoSum/TwoSum.cs
+++ TwoSum/TwoSum.cs
@@ -4,6 +4,34 @@ namespace TwoSum
 {
     public class TwoSum
     {
-        
+        public static bool TryFindIndices(int[] numbers, int target, out int[] result)
+        {
+            if (numbers == null || numbers.Length < 2)
+                throw new ArgumentException("Invalid numbers array passed. At least 2 elements required");
+            
+            Array.Sort(numbers);
+
+            result = new int[2];
+            int leftIndex = 0;
+            int rightIndex = numbers.Length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                var sum = numbers[leftIndex] + numbers[rightIndex];
+                if (sum == target)
+                {
+                    result[0] = rightIndex;
+                    result[1] = leftIndex;
+                    return true;
+                }
+                else if (sum < target) 
+                    leftIndex++;
+                else 
+                    rightIndex--;
+
+            }
+            
+            return false;
+        }
     }
 }
