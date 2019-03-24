c8503c9cebcb4cea542aecf98196a6a5fc501dfe
Marek <frankowicz.marek@gmail.com>
2019-03-24 15:40:51 +0100

    Fix TryFindIndices method

--- TwoSum/TwoSum.cs
+++ TwoSum/TwoSum.cs
@@ -3,33 +3,27 @@ using System.Collections.Generic;
 
 namespace TwoSum
 {
-    public class TwoSum
+    public static class TwoSum
     {
         public static bool TryFindIndices(int[] numbers, int target, out int[] result)
         {
             if (numbers == null || numbers.Length < 2)
                 throw new ArgumentException("Invalid numbers array passed. At least 2 elements required");
-            
-            Array.Sort(numbers);
 
             result = new int[2];
-            int leftIndex = 0;
-            int rightIndex = numbers.Length - 1;
+            var lookup = new HashSet<int>();
 
-            while (leftIndex < rightIndex)
-            {
-                var sum = numbers[leftIndex] + numbers[rightIndex];
-                if (sum == target)
+            for (int i = 0; i < numbers.Length; i++)
+            {  
+                if (lookup.Contains(target - numbers[i]))
                 {
-                    result[0] = rightIndex;
-                    result[1] = leftIndex;
+                    result[0] = i;
+                    result[1] = Array.IndexOf(numbers, target - numbers[i]);
+
                     return true;
                 }
-                else if (sum < target) 
-                    leftIndex++;
-                else 
-                    rightIndex--;
-
+                
+                lookup.Add(numbers[i]);
             }
             
             return false;
